bc6a60c386b99fa4bc035a6b0db7443582e50d38
Katshual <139873320+Katshual@users.noreply.github.com>
2024-09-17 17:20:59 +0200

    Suppression du 's' de biographie dans le titre 'Biographie du Praticien'

--- resources/views/biographies/index.blade.php
+++ resources/views/biographies/index.blade.php
@@ -4,10 +4,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Biographies</title>
+    <title>Biographie</title>
 </head>
 <body>
-    <h1>Biographies du Praticien</h1>
+    <h1>Biographie du Praticien</h1>
 
     @foreach($biographies as $biographie)
         <div>
